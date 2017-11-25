6c7975273fbdf155539dbd43e544fea3aceb58e6
TheXulis <LFGonzalez1997@gmail.com>
2017-11-24 20:40:37 -0800

    Added a window to enter the username and password

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
 
@@ -18,10 +18,45 @@ app.on('ready', function(){
         slashes: true
     }));
 
+    //Quit app when closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
     // Build menu from remplate
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     // Insert menu
     Menu.setApplicationMenu(mainMenu);
+
+    createLoginWindow();
+});
+
+// Handle login window
+function createLoginWindow(){
+    // Create new window
+    loginWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Enter Username'
+    });
+    
+    // Load html into window
+    loginWindow.loadURL(url.format({
+        pathname: path.resolve('./loginWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // Garbage collection handle
+    loginWindow.on('close', function(){
+        addWindow = null;
+    });
+}
+
+// Catch username
+ipcMain.on('login:user', function(e, user){
+    mainWindow.webContents.send('login:user', user);
+    loginWindow.close();
 });
 
 // Create menu template
@@ -30,10 +65,10 @@ const mainMenuTemplate = [
         label: 'File',
         submenu: [
             {
-                label: 'Add Item'
+                label: 'Register'
             },
             {
-                label: 'Clear Items'
+                label: 'Open Chat'
             },
             {
                 label: 'Quit',
@@ -45,4 +80,29 @@ const mainMenuTemplate = [
             }
         ]
     }
-]
+];
+
+// If mac, add an empty object to the menu
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in prod
+if(process.env.Node_ENV != 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu:[
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' :
+                'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role:'reload'
+            }
+        ]
+    });
+}
