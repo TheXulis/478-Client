09f1149f51d2957e2f9ab751687ebd25a812e896
TheXulis <LFGonzalez1997@gmail.com>
2017-12-10 11:16:39 -0800

    Added a .gitignore for files keys that should not be pushed to github

--- Encryption/tester.js
+++ Encryption/tester.js
@@ -1,19 +1,6 @@
 const crypto = require('./encryption');
-const PythonShell = require('python-shell');
 
-var options = {
-    mode: 'text'
-};
-
-PythonShell.run('Encryption/keygen.py', function(err, results){
-    console.log(results)
-    let json = crypto.encryption("A Message");
+let json = crypto.encryption("A Message");
     
-    let plaintext = crypto.decrypt(json);
-    console.log(plaintext);
-})
-
-// let json = crypto.encryption("A Message");
-    
-// let plaintext = crypto.decrypt(json);
-// console.log(plaintext);
+let plaintext = crypto.decrypt(json);
+console.log(plaintext);
